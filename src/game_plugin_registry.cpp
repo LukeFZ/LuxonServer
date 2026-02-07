73f34dde0227acbd8cf4e18c9f5fc3bedcddc358
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2026-02-07 04:35:37 +0100

    LuxonServer: Fix compilation of game_plugin_registry.cpp when using MSVC

--- src/game_plugin_registry.cpp
+++ src/game_plugin_registry.cpp
@@ -14,7 +14,7 @@ std::unordered_map<std::string, PluginFactory> *plugins{};
 
 bool register_(const std::string& name, PluginFactory&& plugin_factory) {
     if (!plugins)
-        plugins = new typeof(*plugins);
+        plugins = new std::unordered_map<std::string, PluginFactory>();
     return plugins->emplace(name, std::move(plugin_factory)).second;
 }
 
