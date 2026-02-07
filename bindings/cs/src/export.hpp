bd0a484dda7222d48a3267af918506c265d5bf50
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2026-02-07 06:00:22 +0100

    expose c api to create, configure and launch server

---
+++ bindings/cs/src/export.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#define CSHARP_API extern "C" __declspec(dllexport)
---
+++ bindings/cs/src/server.cpp
@@ -0,0 +1,48 @@
+#include <cstdint>
+
+#include "export.hpp"
+#include <luxon/server/server_manager.hpp>
+
+struct ServerContext {
+    server::ServerManager manager;
+};
+
+using ServerContextHandle = uintptr_t;
+
+namespace {
+std::unordered_map<ServerContextHandle, std::unique_ptr<ServerContext>> server_contexts;
+std::atomic<ServerContextHandle> next_handle{1};
+}
+
+CSHARP_API ServerContextHandle luxon_csharp_server_context_create() {
+    const auto handle = next_handle.fetch_add(1);
+    server_contexts.emplace(handle, std::make_unique<ServerContext>());
+    return handle;
+}
+
+CSHARP_API void luxon_csharp_server_context_run(const ServerContextHandle handle) {
+    const auto it = server_contexts.find(handle);
+    if (it == server_contexts.end())
+        return;
+    
+    it->second->manager.run();
+}
+
+CSHARP_API void luxon_csharp_server_context_stop(const ServerContextHandle handle) {
+    const auto it = server_contexts.find(handle);
+    if (it == server_contexts.end())
+        return;
+    
+    it->second->manager.stop();
+}
+
+CSHARP_API void luxon_csharp_server_context_configure_server(const ServerContextHandle handle, const char *name, const char *address, const uint16_t port,
+                                                             const bool external) {
+    const auto it = server_contexts.find(handle);
+    if (it == server_contexts.end())
+        return;
+    
+    it->second->manager.configure_server(name, address, port, external);
+}
+
+CSHARP_API void luxon_csharp_server_context_destroy(const ServerContextHandle handle) { server_contexts.erase(handle); }
