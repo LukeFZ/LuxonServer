1dca4fa291faf962e30097d016fa3733dad0ba54
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2026-02-07 06:38:03 +0100

    add example project for c# bindings

---
+++ bindings/cs/LuxonServer.Example/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Net;
+using LuxonServer;
+using LuxonServer.Example;
+
+PluginManager.RegisterPlugin<TestPlugin>("TestPlugin");
+
+var context = ServerContext.Create();
+context.Configure("NameServer", IPEndPoint.Parse("0.0.0.0:5058"));
+context.Configure("MasterServer", IPEndPoint.Parse("0.0.0.0:5055"));
+context.Configure("GameServer", IPEndPoint.Parse("0.0.0.0:5056"));
+
+var thread = new Thread(() =>
+{
+    using (context)
+    {
+        context.Run();
+    }
+});
+thread.Start();
+
+Console.WriteLine("Press Enter to stop the server...");
+Console.ReadLine();
+context.Stop();
---
+++ bindings/cs/LuxonServer.Example/TestPlugin.cs
@@ -0,0 +1,26 @@
+﻿namespace LuxonServer.Example;
+
+public sealed class TestPlugin : IPlugin
+{
+    public TestPlugin()
+    {
+        Console.WriteLine("TestPlugin: created");
+    }
+
+    public PluginResult OnAttach()
+    {
+        Console.WriteLine("TestPlugin: OnAttach");
+        return PluginResult.Continue;
+    }
+
+    public PluginResult OnCreateGame()
+    {
+        Console.WriteLine("TestPlugin: OnCreateGame");
+        return PluginResult.Continue;
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("Disposing TestPlugin");
+    }
+}
